725eb1b154132d62b83b3cce5eecbafec8167c77
manjeetsaini12 <manjeet.itms@gmail.com>
2026-02-25 16:26:54 -0500

    Initial commit: Java app with Docker support

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        Date currentDate = new Date();
+        String name = "Manjeet";
+        System.out.println("Hello " + name + "! Current date: " + currentDate);
+    }
+}
